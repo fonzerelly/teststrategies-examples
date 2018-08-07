d59b0fa5efb38557e33b793350f9116c32e4d84d
cs8433 <christian.hoerauf@consorsbank.de>
2018-08-07 06:43:01 +0200

    Calculating up to 60, about recognizing recursive structure

--- roman-numbers.js
+++ roman-numbers.js
@@ -6,12 +6,16 @@ function range(start, end) {
     return ans;
 }
 
-function serialize(n) {
-    const base = range(0, n);
-    return base.map((_) => {
-        return 'I'
-    }).join('')
+function createSerialize(representation) {
+    return (n) => {
+        const base = range(0, n);
+        return base.map((_) => {
+            return representation
+        }).join('')
+    }
 }
+const serializeOne = createSerialize('I')
+const serializeTen = createSerialize('X')
 
 function createModulo(devider, representation) {
     return (n) => {
@@ -24,14 +28,27 @@ function createModulo(devider, representation) {
 
 const moduloFive = createModulo(5, 'V')
 const moduloTen = createModulo(10, 'X')
+const moduloFifty = createModulo(50, 'L')
 
 module.exports = {
     fromArabicNumber: (n) => {
-        let result = moduloTen(n);
-        if (result[0]) {
-            return result[0] + serialize(result[1])
+        let result = moduloFifty(n);
+        if(result [0]) {
+            const result10 = moduloTen(result[1])
+            if(result10[0]) {
+                const result5 = moduloTen(result[1])
+                return result[0] + result5[0] + serializeOne(result5[1])    
+            }
+            return result[0] + serializeOne(result10[1]);
         }
+        // usecase up to 18
+        result = moduloTen(n);
+        if (result[0]) {
+            const result5 = moduloFive(result[1])
+            return result[0] + result5[0] + serializeOne(result5[1])
+        }
+        // simple use case up to 8 
         result = moduloFive(n)
-        return result[0] + serialize(result[1])
+        return result[0] + serializeOne(result[1])
     }
 }
--- roman-numbers.spec.js
+++ roman-numbers.spec.js
@@ -13,8 +13,17 @@ fdescribe('RomanNumbers', () => {
             { arabic: 11, roman: 'XI'},
             { arabic: 12, roman: 'XII'},
             { arabic: 13, roman: 'XIII'},
+            { arabic: 15, roman: 'XV'},
+            { arabic: 16, roman: 'XVI'},
+            { arabic: 17, roman: 'XVII'},
+            { arabic: 18, roman: 'XVIII'},
+            { arabic: 50, roman: 'L'},
+            { arabic: 51, roman: 'LI'},
+            { arabic: 60, roman: 'LX'},
+            { arabic: 62, roman: 'LXII'}
         ].forEach((testSetup)=> {
             it(`should turn ${testSetup.arabic} to ${testSetup.roman}`, () => {
+                console.log(RomanNumbers.fromArabicNumber(testSetup.arabic))
                 expect(RomanNumbers.fromArabicNumber(testSetup.arabic))
                     .toBe(testSetup.roman);
             })
