1196faf01ae637eed8cd1bac452afff983878fe9
cs8433 <christian.hoerauf@consorsbank.de>
2018-08-14 18:57:41 +0200

    refactor to map

--- ceasars-cipher.js
+++ ceasars-cipher.js
@@ -4,10 +4,9 @@ class CeasarsCipher {
     static encode(shift, text) {
         let key = _alphabet.slice(shift).concat(_alphabet.slice(0, shift))
         let result = "";
-        for (let i = 0; i < text.length; i++) {
-            result += key[_alphabet.indexOf(text.charAt(i))]
-        }
-        return result;
+        return text.split('').map((ch) => {
+            return key[_alphabet.indexOf(ch)]
+        }).join('')
     }
 }
 module.exports = CeasarsCipher
