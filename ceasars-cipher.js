fefe6884c619efd82dfefd057b6291fcd5a27198
cs8433 <christian.hoerauf@consorsbank.de>
2018-08-14 18:50:14 +0200

    refactoring to index lookup

--- ceasars-cipher.js
+++ ceasars-cipher.js
@@ -1,9 +1,11 @@
+const _alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 class CeasarsCipher {
-    // _alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', M];
     static encode(shift, text) {
+        let key = _alphabet.slice(shift).concat(_alphabet.slice(0, shift))
         let result = "";
         for (let i = 0; i < text.length; i++) {
-            result += String.fromCharCode(text.charCodeAt(i) + shift);
+            result += key[_alphabet.indexOf(text.charAt(i))]
         }
         return result;
     }
--- ceasars-cipher.spec.js
+++ ceasars-cipher.spec.js
@@ -5,7 +5,8 @@ fdescribe('ceasars-cipher', () => {
             {shift: 1, text: 'ABC', output: 'BCD'},
             {shift: 1, text: 'HELLOWORLD', output: 'IFMMPXPSME'},
             {shift: 2, text: 'ABC', output: 'CDE'},
-            // {shift: 1, text: 'XYZ', output: 'YZA'},
+            {shift: 1, text: 'XYZ', output: 'YZA'},
+            // {shift: 1, text: 'HELLO WORLD', output: 'IFMMP XPSME'},
 
         ].forEach((setup) => {
             it(`should encode '${setup.text}' to '${setup.output}'`, () => {
